3cafdc343eaf111086181f9addf505d82c3580e3
Nguyễn Văn Hoài <husstress2002@gmail.com>
2025-09-26 21:55:24 +0700

    Completed challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float coolDownTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        coolDownTime -= Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDownTime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDownTime = 1f;
+        }
+    }
+}
